7f5cc352111397b91ceac0cd1cece3bb7c5528f4
Yaroslav Kokoshko <85609406+YarKo04@users.noreply.github.com>
2022-01-05 18:02:16 +0200

    RGR

---
+++ RGR_Calculator_Kokoshko/RGR_Calculator_Kokoshko/RGR_Calculator_KokoshkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGR_Calculator_KokoshkoApp.swift
+//  RGR_Calculator_Kokoshko
+//
+//  Created by Yaroslav Kokoshko on 21.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGR_Calculator_KokoshkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
